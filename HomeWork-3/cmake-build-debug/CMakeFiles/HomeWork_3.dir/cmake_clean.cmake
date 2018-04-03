3df858db4eaad2fb55b0eb92119680397af0a928
Victor <victor.kataev@gmail.com>
2018-04-03 12:48:42 +0200

    new project HomeWork-3

---
+++ HomeWork-3/cmake-build-debug/CMakeFiles/HomeWork_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork_3.dir/main.cpp.o"
+  "HomeWork_3.pdb"
+  "HomeWork_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeWork_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
