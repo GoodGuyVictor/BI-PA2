06376fd9207490656fd10609be81d93ae1ee0ac3
Victor <victor.kataev@gmail.com>
2018-05-08 21:45:36 +0200

    basic concept

--- Semestralka/main.cpp
+++ Semestralka/main.cpp
@@ -1,6 +1,184 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <list>
+#include <string>
+#include <vector>
+#include <typeinfo>
+#include <algorithm>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+class COperatorStack
+{
+
+};
+
+
+class COperand
+{
+protected:
+    string m_value;
+
+public:
+    COperand() = default;
+    explicit COperand(const string & val) : m_value(val) {};
+    virtual ~COperand() = default;
+    COperand & operator+(const COperand & other);
+//    template <typename T>
+//    virtual T getValue() const;
+};
+
+
+class CInteger : public COperand
+{
+public:
+    explicit CInteger(const string & val) : COperand(val) {}
+};
+
+
+class CDecimal : public COperand
+{
+    explicit CDecimal(const string & val) : COperand(val) {}
+
+};
+
+
+class CVariable : public COperand
+{
+private:
+    string m_name;
+public:
+    explicit CVariable(const string & val) : COperand(val) {}
+};
+
+
+class CParser
+{
+private:
+    vector<string> m_output;
+
+    void shuntingYard(const string & input);
+
+public:
+    CParser() = default;
+    ~CParser() = default;
+    void readInput();
+};
+
+void CParser::readInput()
+{
+    string input;
+
+    cout << ">";
+    getline(cin, input, '\n');
+    shuntingYard(input);
+}
+
+void CParser::shuntingYard(const string &input)
+{
+
+}
+
+
+class CCalculator
+{
+private:
+    vector<CVariable> m_variables;
+    vector<string> m_history;
+
+    void removeWhiteSpaces(string &str);
+
+public:
+    CCalculator() { cout << "Welcome to super high precision calculator! Have fun! =)" << endl; }
+    void run();
+    void readInput();
+};
+
+void CCalculator::run()
+{
+    CParser parser;
+
+    readInput();
+//    while(true)
+//    {
+//        parser.readInput();
+//    }
+}
+
+void CCalculator::readInput()
+{
+    string input;
+
+    cout << ">";
+    getline(cin, input, '\n');
+    removeWhiteSpaces(input);
+
+    cout << "Read input: " << input;
+
+    if(input.find('=') != string::npos)
+    {
+
+    }
+}
+
+void CCalculator::removeWhiteSpaces(string &str)
+{
+    for (auto it = str.begin(); it < str.end(); it++)
+        if (*it == ' ')
+            str.erase(it);
+}
+
+void extractInt(const string & str);
+
+int main()
+{
+
+
+
+//    string sinput("53.345747");
+//    string::size_type sz;
+//    double dinput = stod(sinput);
+//
+//    cout << dinput;
+//
+//    do
+//    {
+//        getline(cin, input, '\n');
+//        cout << input << endl;
+//        extractInt(input);
+//
+//    } while(input != "quit");
+//
+//
+    CCalculator calc;
+    calc.run();
+
     return 0;
+}
+
+void extractInt(const string & str)
+{
+    stringstream ss;
+    vector<int> tmpCoords;
+
+    /* Storing the whole string into string stream */
+    ss << str;
+
+    /* Running loop till the end of the stream */
+    string temp;
+    int found;
+    while (!ss.eof()) {
+
+        /* extracting word by word from stream */
+        ss >> temp;
+
+        /* Checking the given word is integer or not */
+        if (stringstream(temp) >> found) {
+            tmpCoords.push_back(found);
+        }
+    }
+    cout << "extracted ints:" << endl;
+    for (auto &it : tmpCoords)
+        cout << it << " ";
 }
