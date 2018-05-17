c4723eb11029aab938aeb5f3474f6c96d1f8b0bb
Victor <victor.kataev@gmail.com>
2018-05-17 15:18:41 +0200

    Created class CVariable

--- Semestralka/CVariable.h
+++ Semestralka/CVariable.h
@@ -5,4 +5,17 @@
 #ifndef SEMESTRALKA_CVARIABLE_H
 #define SEMESTRALKA_CVARIABLE_H
 
+#include <string>
+
+class CVariable
+{
+public:
+    CVariable (const std::string &name, const std::string val)
+            : m_name(name), m_value(val) {}
+
+private:
+    std::string m_name;
+    std::string m_value;
+};
+
 #endif //SEMESTRALKA_CVARIABLE_H
