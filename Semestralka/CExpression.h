e01753560a9cba0f02c248f571a5520e1feb3e36
Victor <victor.kataev@gmail.com>
2018-05-17 15:14:30 +0200

    Created abstract class CExpression

--- Semestralka/CExpression.h
+++ Semestralka/CExpression.h
@@ -5,4 +5,16 @@
 #ifndef SEMESTRALKA_CEXPRESSION_H
 #define SEMESTRALKA_CEXPRESSION_H
 
+#include "CBigNum.h"
+
+class CExpression
+{
+public:
+    CExpression() = default;
+    virtual ~CExpression() = default;
+
+    virtual CBigNum evaluate() const = 0;
+};
+
+
 #endif //SEMESTRALKA_CEXPRESSION_H
