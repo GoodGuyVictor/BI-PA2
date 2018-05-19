3cc183dad91bbfd858e979e6616fdc7bce8c07af
Victor <victor.kataev@gmail.com>
2018-05-19 12:15:52 +0200

    Implemented method CLongInteger::toBigInt(const std::string & text) Updated constructor of class CLongInteger

--- Semestralka/CLongInteger.h
+++ Semestralka/CLongInteger.h
@@ -10,17 +10,50 @@
 class CLongInteger : public CExpression
 {
 public:
-    CLongInteger(const std::string & val)
+    CLongInteger(std::string & val)
     {
-        std::stringstream ss;
-        ss << val;
-        ss >> m_value;
+        if(val[0] == '-') {
+            m_sgn = true;
+            val.erase(0);
+        }
+        m_value = toBigInt(val);
     }
     CBigNum evaluate() const override ;
 
 private:
     bool m_sgn;
     std::vector<uint32_t> m_value;
+
+    std::vector<uint32_t> toBigInt(const std::string &);
 };
+
+std::vector<uint32_t> CLongInteger::toBigInt(const std::string & text)
+{
+    // convert string to BCD-like
+    for (char &c : text) c -= '0';
+    // build result vector
+    std::vector<uint32_t> value(1, 0);
+    uint32_t bit = 1;
+    for (;;) {
+        // set next bit if last digit is odd
+        if (text.back() & 1) value.back() |= bit;
+        // divide BCD-like by 2
+        bool notNull = false; int carry = 0;
+        for (char &c : text) {
+            const int carryNew = c & 1;
+            c /= 2; c += carry * 5;
+            carry = carryNew;
+            notNull |= c;
+        }
+        if (!notNull) break;
+        // shift bit
+        bit <<= 1;
+        if (!bit) {
+            value.push_back(0); bit = 1;
+        }
+    }
+    // done
+    return value;
+}
 
 #endif //SEMESTRALKA_CLONGINTEGER_H
