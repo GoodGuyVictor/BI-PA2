0492ddac067e64583a761c647c24edbaf4cc47cd
Victor <victor.kataev@gmail.com>
2018-05-20 11:49:44 +0200

    Created default cosntructor in class CBigNum. Updated constructor CBigNum::CBigNum(int val) Created and implemented constructor CBigNum::CBigNum(double val)

--- Semestralka/CBigNum.h
+++ Semestralka/CBigNum.h
@@ -5,18 +5,59 @@
 #ifndef SEMESTRALKA_CBIGNUM_H
 #define SEMESTRALKA_CBIGNUM_H
 
+#include <sstream>
+
 class CBigNum
 {
 public:
-    CBigNum(const int val)
+    CBigNum() = default;
+
+    CBigNum(int val)
     {
-        m_sgn = val < 0;
-        m_value.push_back((uint32_t)val);
+        if(val < 0) {
+            m_sgn = true;
+            val *= -1;
+        } else
+            m_sgn = false;
+        m_exponent.push_back((uint32_t)val);
+    }
+
+    CBigNum(double val)
+    {
+        if(val < 0) {
+            m_sgn = true;
+            val *= -1;
+        } else
+            m_sgn = false;
+
+        std::string dStr = std::to_string(val);
+
+        char * token;
+        token = strtok((char*)dStr.c_str(), ".");
+        std::stringstream ss;
+        ss << token;
+        uint32_t exponent;
+        ss >> exponent;
+        m_exponent.push_back(exponent);
+        ss.clear();
+
+        token = strtok(NULL, ".");
+        ss << token;
+        uint32_t fraction;
+        ss >> fraction;
+        m_fraction.push_back(fraction);
+    }
+
+    CBigNum & operator+ (const CBigNum & other)
+    {
+        return *this;
     }
 
 private:
-    std::vector<uint32_t> m_value;
+    std::vector<uint32_t> m_exponent;
+    std::vector<uint32_t> m_fraction;
     bool m_sgn;
+    size_t m_fractionPos;
 };
 
 #endif //SEMESTRALKA_CBIGNUM_H
