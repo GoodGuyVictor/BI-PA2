03099de62f21503f553b64901f3fc4c646acdfbd
Victor <victor.kataev@gmail.com>
2018-05-23 09:32:06 +0200

    Added constructor implementation

--- Semestralka/CInteger.cpp
+++ Semestralka/CInteger.cpp
@@ -4,6 +4,13 @@
 
 #include "CInteger.h"
 
+CInteger::CInteger(const std::string &val)
+{
+    std::stringstream ss;
+    ss << val;
+    ss >> m_value;
+}
+
 CBigNum CInteger::evaluate() const
 {
     return CBigNum(m_value);
