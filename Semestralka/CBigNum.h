9fcd241b40246b4be582aa734b156fe9ffedaf48
Victor <victor.kataev@gmail.com>
2018-05-24 15:18:31 +0200

    Created methods: std::string toString (const std::vector<uint32_t> &) const; std::string toString (const CBigNum &) const; std::string toString (uint32_t) const; void divideAlgorithm (const std::vector<uint32_t> &,                       const std::vector<uint32_t> &,                       std::string &, std::string &) const; void multiplyByTen (); int  eliminateEndingZeros (std::vector<uint32_t> &,                            std::vector<uint32_t> &) const; std::vector<uint32_t> toBigInt (std::string & text) const; CBigNum multiplicationAlgorithm (const std::vector<uint32_t> &,                                  const std::vector<uint32_t> &) const;

--- Semestralka/CBigNum.h
+++ Semestralka/CBigNum.h
@@ -13,26 +13,39 @@ class CBigNum
 {
 public:
                 CBigNum     ()
-                        : m_sgn(false), m_fraction(0);
+                        : m_sgn(false), m_fraction(0) {};
     explicit    CBigNum     (int val);
     explicit    CBigNum     (double val);
                 CBigNum     (bool sgn, const std::vector<uint32_t > & val);
 
     void            print           () const;
-    CBigNum &       operator+       (const CBigNum & other);
-    CBigNum &       operator-       (const CBigNum & other);
+    CBigNum &       operator+       (const CBigNum &);
+    CBigNum &       operator-       (const CBigNum &);
     CBigNum &       operator-       ();
-    CBigNum         operator*       (const CBigNum & other);
+    CBigNum         operator*       (const CBigNum &);
+    CBigNum         operator/       (const CBigNum &);
+    bool            operator>=      (const CBigNum &);
 
 private:
     std::vector<uint32_t>   m_exponent;
     uint32_t                m_fraction;
     bool                    m_sgn;
 
-    uint32_t        addFractions                (uint32_t f1, uint32_t f2, unsigned short & carry) const;
-    uint32_t        subtractFractions           (uint32_t f1, uint32_t f2, unsigned short & carry);
-    unsigned short  getCarry                    (const uint32_t sum) const;
-    void            expandFewerNumberWithZeros  (std::vector<uint32_t> & exponent1, std::vector<uint32_t> & exponent2);
+    uint32_t        addFractions                (uint32_t, uint32_t, unsigned short &) const;
+    uint32_t        subtractFractions           (uint32_t, uint32_t, unsigned short &);
+    unsigned short  getCarry                    (const uint32_t) const;
+    void            expandFewerNumberWithZeros  (std::vector<uint32_t> &, std::vector<uint32_t> &);
+    std::string     toString                    (const std::vector<uint32_t> &) const;
+    std::string     toString                    (const CBigNum &) const;
+    std::string     toString                    (uint32_t) const;
+    void            divideAlgorithm             (const std::vector<uint32_t> &,
+                                                 const std::vector<uint32_t> &,
+                                                 std::string &, std::string &) const;
+    void            multiplyByTen               ();
+    int            eliminateEndingZeros         (std::vector<uint32_t> &, std::vector<uint32_t> &) const;
+    std::vector<uint32_t> toBigInt              (std::string & text) const;
+    CBigNum         multiplicationAlgorithm     (const std::vector<uint32_t> &,
+                                                 const std::vector<uint32_t> &) const;
 };
 
 #endif //SEMESTRALKA_CBIGNUM_H
