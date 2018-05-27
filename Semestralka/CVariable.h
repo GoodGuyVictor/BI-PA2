3d727a53395f2b3e53ac9783941298fdfe4b5bba
Victor <victor.kataev@gmail.com>
2018-05-27 11:43:22 +0200

    -CVariable now inherits CExpression -Added new constructor CVariable (const std::string &name, bool, const std::vector<uint32_t> &, const std::vector<uint32_t> &) -Added methods getName() const, clone() const

--- Semestralka/CVariable.h
+++ Semestralka/CVariable.h
@@ -6,16 +6,25 @@
 #define SEMESTRALKA_CVARIABLE_H
 
 #include <string>
+#include "CExpression.h"
 
-class CVariable
+class CVariable : public CExpression
 {
 public:
-    CVariable (const std::string &name, const std::string val)
-            : m_name(name), m_value(val) {}
+    CVariable (const std::string &name, const std::string & val);
+    CVariable (const std::string &name, bool, const std::vector<uint32_t> &, const std::vector<uint32_t> &);
+
+    CBigNum evaluate() const override;
+    std::string getName() const;
+    CExpression * clone() const;
 
 private:
     std::string m_name;
-    std::string m_value;
+    bool m_sgn;
+    std::vector<uint32_t> m_exponent;
+    std::vector<uint32_t> m_fraction;
+
+    std::vector<uint32_t> toBigInt(std::string text) const;
 };
 
 #endif //SEMESTRALKA_CVARIABLE_H
