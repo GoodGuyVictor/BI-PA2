970d015aac519e989040a0f5598aaed8c435b4c6
Victor <victor.kataev@gmail.com>
2018-05-28 10:11:40 +0200

    All exceptions now inherits from std::exception and have virtual method what() implemented

--- Semestralka/Exceptions.h
+++ Semestralka/Exceptions.h
@@ -1,4 +1,29 @@
-class InvalidInput{};
-class InvalidVariableName{};
-class InvalidValue{};
-class MissingParanthesis{};
+class InvalidInput : public std::exception
+{
+    const char * what() const throw() override
+    {
+        return "Invalid input";
+    }
+};
+
+class InvalidVariableName : public std::exception
+{
+    const char * what() const throw() override
+    {
+        return "Invalid variable name";
+    }
+};
+class InvalidValue : public std::exception
+{
+    const char * what() const throw() override
+    {
+        return "Invalid Value";
+    }
+};
+class MissingParanthesis : public std::exception
+{
+    const char * what() const throw() override
+    {
+        return "Missing paranthesis";
+    }
+};
