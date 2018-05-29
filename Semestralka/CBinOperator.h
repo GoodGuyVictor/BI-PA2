0b5903d7e602813ddc8763dce6bdaaa1944ef652
Victor <victor.kataev@gmail.com>
2018-05-29 15:17:59 +0200

    Documented CBinOperator

--- Semestralka/CBinOperator.h
+++ Semestralka/CBinOperator.h
@@ -7,11 +7,26 @@
 
 #include "CExpression.h"
 
+/*! @class CBinOperator
+ *  @brief The class simulates binary operations
+ *
+ */
+
 class CBinOperator : public CExpression
 {
 public:
+    /**
+     * Constructor
+     * @param l left hand operand
+     * @param r right hand operand
+     * all parameters allocated dynamically
+     */
     CBinOperator(CExpression * l, CExpression * r)
             : m_lVal(l), m_rVal(r) {};
+    /**
+     * Destructor
+     * Frees up allocated parameters
+     */
     ~CBinOperator()
     {
         delete m_lVal;
@@ -19,7 +34,9 @@ public:
     };
 
 protected:
+    /** left hand operand */
     CExpression * m_lVal;
+    /** right hand operand */
     CExpression * m_rVal;
 };
 
