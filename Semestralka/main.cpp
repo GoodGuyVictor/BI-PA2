679d59c03e90f2598d318922dfd9a8eb0f8d5410
Victor <victor.kataev@gmail.com>
2018-05-29 15:19:05 +0200

    Project files for building

--- Semestralka/main.cpp
+++ Semestralka/main.cpp
@@ -7,18 +7,6 @@ int main()
 
     CCalculator calc;
     calc.run();
-    calc.saveToFile();
-
-//    std::cout << 34658 % 839 << std::endl;
-
-//    34765238 + 9283458 - (738468 * 432 / 235 - (47683 + 683) * 4378 - 1)
 
     return 0;
 }
-
-
-//todo list
-//-input validation
-//-exceptions
-
-//((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1))
