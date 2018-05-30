32fd3b198271244d5cfc8ec8d4aa551fea9877d9
Victor <victor.kataev@gmail.com>
2018-05-30 13:36:12 +0200

    Documented class CAddExp

--- Semestralka/CAddExp.h
+++ Semestralka/CAddExp.h
@@ -7,12 +7,24 @@
 
 #include "CBinOperator.h"
 
+/*! @class CAddExp
+ *  @brief The class adds up two expressions
+ */
 class CAddExp : public CBinOperator
 {
 public:
+ /**
+  * Constructor creates new addition expression
+  * @param l Left hand operand
+  * @param r Right hand operand
+  */
             CAddExp     (CExpression * l, CExpression * r)
                     : CBinOperator(l, r) {};
 
+    /**
+     * Evaluates the sum of two expressions
+     * @return Highly accurate number
+     */
     CBigNum evaluate    () const override ;
 };
 
