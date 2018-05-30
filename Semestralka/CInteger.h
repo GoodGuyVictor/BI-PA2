3f8ef5c24ddcf773f2c59137e697b2620fc35e19
Victor <victor.kataev@gmail.com>
2018-05-30 14:57:10 +0200

    Documented class CInteger

--- Semestralka/CInteger.h
+++ Semestralka/CInteger.h
@@ -8,14 +8,25 @@
 #include <sstream>
 #include "CExpression.h"
 
+/*! @class CInteger
+ *  @brief The class represents integer number. Child class of CExpression.
+ */
 class CInteger : public CExpression
 {
 public:
-    explicit    CInteger   (const std::string & val);
-
+    /**
+     * Cpnstructor creates integer object
+     * @param value String value to be translated into CInteger
+     */
+    explicit    CInteger   (const std::string & value);
+    /**
+     * Translates CInteger into CBigNum
+     * @return CBigNum object
+     */
     CBigNum     evaluate   () const override ;
 
 private:
+    /** Signed integer value */
     int m_value;
 };
 
