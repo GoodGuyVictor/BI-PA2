ecb997fc5a12788e70011260e16a96d8b0464d28
Victor <victor.kataev@gmail.com>
2018-05-30 14:57:42 +0200

    Documented CLongInteger class

--- Semestralka/CLongInteger.h
+++ Semestralka/CLongInteger.h
@@ -8,21 +8,34 @@
 #include <algorithm>
 #include "CExpression.h"
 
+/*! @class CLongInteger
+ *  @brief The class represents long integer number. Child class of CExpression.
+ */
 class CLongInteger : public CExpression
 {
 public:
-    explicit    CLongInteger    (std::string val);
+    /**
+     * Constructor creates long integer object
+     * @param value String value to be translated into CLongInteger
+     */
+    explicit    CLongInteger    (std::string value);
+    /**
+     * Translates CLongInteger into CBigNum
+     * @return CBigNum object
+     */
     CBigNum     evaluate        () const override ;
 
 private:
+    /** Presence of minus */
     bool                    m_sgn;
+    /** Huge integer number placed into vector of uint32_t's */
     std::vector<uint32_t>   m_value;
-
-    std::vector<uint32_t>   toBigInt(std::string &);
+    /**
+     * Translates string value into vector of uint32_t's
+     * @param value String value to be translated
+     * @return Vector of uint32_t's
+     */
+    std::vector<uint32_t>   toBigInt(std::string & value);
 };
-
-
-
-
 
 #endif //SEMESTRALKA_CLONGINTEGER_H
