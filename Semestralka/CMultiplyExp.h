edf2b3f9502cd64a2d96c302ae018710d008af41
Victor <victor.kataev@gmail.com>
2018-05-30 14:58:04 +0200

    Documented CMultiplyExp class

--- Semestralka/CMultiplyExp.h
+++ Semestralka/CMultiplyExp.h
@@ -7,10 +7,22 @@
 
 #include "CBinOperator.h"
 
+/*! @class CMultiplyExp
+ *  @brief The class represents expression that performs multiplication operation. Child class of CExpression.
+ */
 class CMultiplyExp : public CBinOperator
 {
 public:
+    /**
+     * Constructor creates new multiplication expression
+     * @param l Left hand operand
+     * @param r Right hand operand
+     */
     CMultiplyExp(CExpression * l, CExpression * r) : CBinOperator(l, r) {};
+    /**
+     * Evaluates the product of two expressions
+     * @return Highly accurate number
+     */
     CBigNum evaluate() const override ;
 };
 
