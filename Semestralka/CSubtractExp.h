a4df3dfda03ded954eebf37fb617ff8fa1e2681f
Victor <victor.kataev@gmail.com>
2018-05-30 14:59:03 +0200

    Documented class CSubtractExp

--- Semestralka/CSubtractExp.h
+++ Semestralka/CSubtractExp.h
@@ -7,12 +7,23 @@
 
 #include "CBinOperator.h"
 
+/*! @class CSubtractExp
+ *  @brief The class represents expression that performs subtraction operation. Child class of CExpression.
+ */
 class CSubtractExp : public CBinOperator
 {
 public:
+      /**
+      * Constructor creates new subtraction expression
+      * @param l Left hand operand
+      * @param r Right hand operand
+      */
             CSubtractExp    (CExpression * l, CExpression * r)
                     : CBinOperator(l, r) {};
-
+    /**
+     * Evaluates the difference of two expressions
+     * @return Highly accurate number
+     */
     CBigNum evaluate        () const override ;
 };
 
