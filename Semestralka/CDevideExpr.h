e1c9bb69c9d247e2d9157c4b016f5272cf6732f4
Victor <victor.kataev@gmail.com>
2018-05-30 14:56:41 +0200

    Documented CDevideExpr class

--- Semestralka/CDevideExpr.h
+++ Semestralka/CDevideExpr.h
@@ -7,11 +7,25 @@
 
 #include "CBinOperator.h"
 
+/*! @class CDevideExp
+ *  @brief The class represents expression that performs division operation taking quotient only and discards
+ *  remainder. Child class of CExpression.
+ *
+ */
 class CDevideExp : public CBinOperator
 {
 public:
+    /**
+     * Constructor creates division expression
+     * @param l Left hand operand
+     * @param r Right hand operand
+     */
             CDevideExp  (CExpression * l, CExpression * r)
                     : CBinOperator(l, r) {};
+    /**
+     * Evaluates the division operation
+     * @return Quotient of the division represented as CBigNum
+     */
     CBigNum evaluate    () const override ;
 };
 
