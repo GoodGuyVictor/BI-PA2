02081fb8a0c04e1ddcd129a313a4825d0b5e399a
Victor <victor.kataev@gmail.com>
2018-05-30 13:46:06 +0200

    Refactor: class name, m_exponent -> m_integer, toBigInt parameter name text -> value

--- Semestralka/CDecimal.cpp
+++ Semestralka/CDecimal.cpp
@@ -4,7 +4,7 @@
 
 #include "CDecimal.h"
 
-Decimal::Decimal(const std::string & val)
+CDecimal::CDecimal(const std::string & val)
 {
     std::string value(val);
     if(value[0] == '-') {
@@ -19,33 +19,33 @@ Decimal::Decimal(const std::string & val)
     token = strtok(NULL, ".");
     std::string fraction(token);
 
-    m_exponent = toBigInt(exponent);
+    m_integer = toBigInt(exponent);
     m_fraction = toBigInt(fraction);
 }
 
-CBigNum Decimal::evaluate() const
+CBigNum CDecimal::evaluate() const
 {
-    return CBigNum(m_sgn, m_exponent, m_fraction);
+    return CBigNum(m_sgn, m_integer, m_fraction);
 }
 
-std::vector<uint32_t> Decimal::toBigInt(std::string & text) const
+std::vector<uint32_t> CDecimal::toBigInt(std::string & value) const
 {
     std::vector<uint32_t> result;
-    int len = text.size();
+    int len = value.size();
     int portions = len / 9;
     std::stringstream ss;
     uint32_t tmp;
 
     for(int i = 0; i < portions; i++) {
-        ss << text.substr(text.size() - 9, 9);
+        ss << value.substr(value.size() - 9, 9);
         ss >> tmp;
         result.push_back(tmp);
-        text.erase(text.size() - 9, 9);
+        value.erase(value.size() - 9, 9);
         ss.clear();
     }
 
-    if(!text.empty()) {
-        ss << text;
+    if(!value.empty()) {
+        ss << value;
         ss >> tmp;
         result.push_back(tmp);
     }
